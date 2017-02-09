0813618d2d6dec55409630af0d8d1e0698a120e9
Andrew Jaros <amjaros1@gmail.com>
2017-02-09 11:24:53 -0500

    add VM and VMI

--- m1-w4d4-vending-minicapstone/Vend/Classes/VendMachine.cs
+++ m1-w4d4-vending-minicapstone/Vend/Classes/VendMachine.cs
@@ -8,5 +8,36 @@ namespace Vend.Classes
 {
     public class VendMachine
     {
+        Dictionary<string, VendingMachineItems> machine = new Dictionary<string, VendingMachineItems>();
+
+        public void Vend()
+        {
+
+            while (true)
+            {
+                Console.WriteLine("1) Feed Money");
+                Console.WriteLine("2) Purchase Items");
+                int userInput = int.Parse(Console.ReadLine());
+
+                if(userInput == 1)
+                {
+                    // feed money method
+                }
+                else if (userInput == 2)
+                {
+                    // purchase items method
+                }
+                else if (userInput == 0)
+                {
+                    // technician method
+                }
+                else if(userInput == 9)
+                {
+                    // exit loop
+                    break;
+                }
+
+            }
+        }
     }
 }
--- m1-w4d4-vending-minicapstone/Vend/Classes/VendingMachineItems.cs
+++ m1-w4d4-vending-minicapstone/Vend/Classes/VendingMachineItems.cs
@@ -8,6 +8,27 @@ namespace Vend.Classes
 {
     public class VendingMachineItems
     {
-        
+        private string slot = "";
+        private string name = "";
+        private double price = 0.00;
+        private int quantityRemaining = 0;
+
+        public string Slot
+        {
+            get { return slot; }
+        }
+        public string Name
+        {
+            get { return name; }
+        }
+        public double Price
+        {
+            get { return price; }
+        }
+        public int QuantityRemaining
+        {
+            get { return quantityRemaining; }
+        }
+
     }
 }
